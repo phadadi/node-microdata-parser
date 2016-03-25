210cadd9bb95f78c04bb4cc2f471c348b1f39f15
Hadadi Péter <peter.hadadi@bigfish.hu>
2016-03-25 17:00:21 +0100

    Multiple additionalProperty

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
 //var request = require('request');
 var htmlparser = require("htmlparser2");
 
-function parse(data, cb) {  
-  cb = cb || function() {};
+function parse(data, cb) {
+  cb = cb || function () {};
   var items = [];
   var scopes = [];
   var getNextText = null;
   var tags = [];
-
+  var props = {};
+  var propType = "http://schema.org/PropertyValue";
+  
   var parser = new htmlparser.Parser({
-    onopentag: function(name, attribs){
+    onopentag: function (name, attribs) {
       var scope = scopes.length && scopes[scopes.length - 1];
-      
-      if(attribs.hasOwnProperty('itemscope')) {
+
+      if (attribs.hasOwnProperty('itemscope')) {
         // create a new scope
         if (attribs.itemprop && scopes.length) {
           // chain the scopes
           scope = scope[attribs.itemprop] = {};
-        }
-        else {
+        } else {
           scope = {};
         }
         scopes.push(scope);
         tags.push('SCOPE');
-      }
-      else {
+      } else {
         tags.push(false);
       }
 
       if (scope) {
-        if(attribs.itemtype) {
+        if (attribs.itemtype) {
           scope.type = attribs.itemtype;
         }
-        if(attribs.itemprop && !attribs.hasOwnProperty('itemscope')) {
+        if (attribs.itemprop && !attribs.hasOwnProperty('itemscope')) {
           if (attribs.content) {
             scope[attribs.itemprop] = attribs.content;
-          }
-          else {
+          } else if (attribs.href) {
+            scope[attribs.itemprop] = attribs.href;
+          } else if (attribs.src) {
+            scope[attribs.itemprop] = attribs.src;
+          } else {
             tags.pop();
             tags.push('TEXT');
             scope[attribs.itemprop] = '';
@@ -45,27 +48,39 @@ function parse(data, cb) {
         }
       }
     },
-    ontext: function(text) {
+    ontext: function (text) {
       if (getNextText) {
         scopes[scopes.length - 1][getNextText] += text.replace(/^\s+|\s+$/g, "");
       }
     },
-    onclosetag: function(tagname){
+    onclosetag: function (tagname) {
       var tag = tags.pop();
-      if(tag === 'SCOPE') {
+      if (tag === 'SCOPE') {
         var item = scopes.pop();
+
+        if (typeof item === 'object' && item.hasOwnProperty('type') && item.type === propType) {
+          props[item.name] = item.value;
+        }
+        if (typeof item === 'object' && item.hasOwnProperty('type')) {
+          delete item.type;
+        }
         if (!scopes.length) {
+          for (var prop in props) {
+            if (props.hasOwnProperty(prop)) {
+              item[prop] = props[prop];
+            }
+          }
+          delete item.additionalProperty;
           items.push(item);
         }
-      }
-      else if (tag === 'TEXT') {
+      } else if (tag === 'TEXT') {
         getNextText = false;
       }
     },
-    onerror: function(err) {
+    onerror: function (err) {
       cb(err);
     },
-    onend: function() {
+    onend: function () {
       cb(null, items);
     }
   });
@@ -75,16 +90,16 @@ function parse(data, cb) {
 }
 
 /*function parseUrl(url, cb) {
-  cb = cb || function() {};
-
-  request(url, function(err, res, body) {
-    if (err) {
-      return cb(err);
-    }
-
-    parse(body, cb);
-  });
-}*/
+ cb = cb || function() {};
+ 
+ request(url, function(err, res, body) {
+ if (err) {
+ return cb(err);
+ }
+ 
+ parse(body, cb);
+ });
+ }*/
 
 exports.parse = parse;
 //exports.parseUrl = parseUrl;
